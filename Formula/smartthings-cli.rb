5ade640e75a12f87bc81528b91b86cf62f9e9702
Ross Stenersen <rossiam@gmail.com>
2022-05-18 12:48:22 -0500

    update to beta.6

--- Formula/smartthings-cli.rb
+++ Formula/smartthings-cli.rb
@@ -3,7 +3,7 @@ require "language/node"
 class SmartthingsCli < Formula
   desc "CLI for developing with SmartThings"
   homepage "https://github.com/SmartThingsCommunity/smartthings-cli"
-  version '1.0.0-beta.5'
+  version '1.0.0-beta.6'
   url "file:///dev/null"
   sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "Apache-2.0"
@@ -39,7 +39,7 @@ class SmartthingsCli < Formula
     # system "npm", "ls", "--global", "--prefix=#{libexec}"
     # puts "\n\n*** installing now:\n\n"
     # stdout, stderr, status = Open3.capture3("npm install #{args}")
-    system "npm", "install", "--global", "--prefix=#{libexec}", "@smartthings/cli@1.0.0-beta.5"
+    system "npm", "install", "--global", "--prefix=#{libexec}", "@smartthings/cli@1.0.0-beta.6"
     # stdout, stderr, status = Open3.capture3("npm install #{Language::Node.std_npm_install_args(libexec).join(' ')}")
     # puts "\n\ninstall status = #{status}"
     # puts "\n\ninstall stdout:\n#{stdout}"
@@ -51,6 +51,6 @@ class SmartthingsCli < Formula
 
   test do
     version_info = pipe_output("#{bin}/smartthings --version").split[0]
-    assert version_info == "@smartthings/cli/1.0.0-beta.5"
+    assert version_info == "@smartthings/cli/1.0.0-beta.6"
   end
 end
